3437b0de6db3f8c6bec912553ef7b331a5c3852f
Martin Schlender <martinschlender@Mac-mini-von-Martin.local>
2026-02-16 08:33:29 +0100

    CapsWord for German with KC_SLASH as -_

--- keyboards/keebart/corne_mx_pro/keymaps/vial/keymap.c
+++ keyboards/keebart/corne_mx_pro/keymaps/vial/keymap.c
@@ -363,3 +363,22 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, 5, 6, 7); // neu (Mac)
     return state;
 }
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_SLASH:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
